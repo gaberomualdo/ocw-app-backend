4cbbec0a84b8c72ce9ecc0655e1a5d3e30c1ccfe
xtrp <36894883+xtrp@users.noreply.github.com>
2021-07-17 01:54:39 -0400

    Rethink Course and Lecture models

--- functions/src/models/Course.ts
+++ functions/src/models/Course.ts
@@ -6,90 +6,64 @@ type Tab = {
   url: string;
   subTabs?: Tab[];
 };
+
 export default class Course extends Model {
   collectionName: string;
+  data: {
+    url: string;
+    title: string;
+    instructors: string[];
+    description: string;
+    imageURL: string;
+    imageCaption: string;
 
-  url: string;
-  title: string;
-  instructors: string[];
-  description: string;
-  imageURL: string;
-  imageCaption: string;
+    hasLectures: boolean;
+    tabs: Tab[];
 
-  hasLectures: boolean;
-  tabs: Tab[];
-
-  semesterTaught: string;
-  level: string;
-  location: {
-    topic: string;
+    semesterTaught: string;
+    level: string;
     department: string;
-    category: string;
+    locations: {
+      topic: string;
+      category: string;
+      specialty: string;
+    }[];
+    sortAs: string;
+    originalOCWData: any;
   };
-  sortAs: string;
-  originalOCWData: any;
 
   constructor(data: any, id?: string) {
     super(id);
     this.collectionName = 'courses';
 
-    this.instructors = data.instructors;
-    this.description = data.description;
-    this.imageURL = data.imageURL;
-    this.imageCaption = data.imageCaption;
-    this.hasLectures = data.hasLectures;
-    this.tabs = data.tabs;
-
-    this.url = data.url;
-    this.semesterTaught = data.semesterTaught;
-    this.originalOCWData = data.originalOCWData || {};
-    this.level = data.level;
-    this.location = data.location;
-    this.title = data.title;
-    this.sortAs = data.sortAs;
-  }
-
-  static getDataFromOCWJSON(json: any) {
-    // get partial data from the OCW JSON -- additional data like instructors and tabs should be added manually
-    return {
-      url: json.href,
-      semesterTaught: json.sem,
-      level: json.level,
-      location: {
-        department: json.department,
-        category: json.topics[0]?.subCat || 'N/A',
-      },
-      title: json.title,
-      sortAs: json.sort_as,
-      originalOCWData: json,
+    this.data = {
+      instructors: data.instructors,
+      description: data.description,
+      imageURL: data.imageURL,
+      imageCaption: data.imageCaption,
+      hasLectures: data.hasLectures,
+      tabs: data.tabs,
+      url: data.url,
+      semesterTaught: data.semesterTaught,
+      originalOCWData: data.originalOCWData || {},
+      level: data.level,
+      department: data.department,
+      locations: data.locations,
+      title: data.title,
+      sortAs: data.sortAs,
     };
   }
 
   toJSON() {
     // represents how this model is stored in Firestore
-    return {
-      url: this.url,
-      title: this.title,
-      semesterTaught: this.semesterTaught,
-      level: this.level,
-      sortAs: this.sortAs,
-      location: this.location,
-      originalOCWData: this.originalOCWData,
-
-      instructors: this.instructors,
-      description: this.description,
-      imageURL: this.imageURL,
-      imageCaption: this.imageCaption,
-      hasLectures: this.hasLectures,
-      tabs: this.tabs,
-    };
+    return this.data;
   }
 
   save(): Promise<void> {
     return new Promise((resolve, reject) => {
-      saveToFirestore(this.collectionName, this.id, this.toJSON()).then(() => {
-        resolve();
-      });
+      saveToFirestore(this.collectionName, this.id, this.toJSON())
+        .catch((err) => reject(err))
+        .then(() => resolve());
     });
   }
 }
--- functions/src/models/Lecture.ts
+++ functions/src/models/Lecture.ts
@@ -4,47 +4,42 @@ import Model from './Model';
 export default class Lecture extends Model {
   collectionName: string;
 
-  url: string;
-  title: string;
-  courseID: string;
-  lectureNumber: number;
-  description: string;
-  instructors: string[];
-  thumbnailURL: string;
-  lectureNotesHTML: string;
+  data: {
+    url: string;
+    title: string;
+    courseID: string;
+    lectureNumber: number;
+    description: string;
+    instructors: string[];
+    thumbnailURL: string;
+    lectureNotesHTML: string;
+  };
 
   constructor(data: any, id?: string) {
     super(id);
     this.collectionName = 'lectures';
 
-    this.url = data.url;
-    this.title = data.title;
-    this.courseID = data.courseID;
-    this.lectureNumber = data.lectureNumber;
-    this.description = data.description;
-    this.instructors = data.instructors;
-    this.thumbnailURL = data.thumbnailURL;
-    this.lectureNotesHTML = data.lectureNotesHTML;
+    this.data = {
+      url: data.url,
+      title: data.title,
+      courseID: data.courseID,
+      lectureNumber: data.lectureNumber,
+      description: data.description,
+      instructors: data.instructors,
+      thumbnailURL: data.thumbnailURL,
+      lectureNotesHTML: data.lectureNotesHTML,
+    };
   }
 
   toJSON() {
-    return {
-      url: this.url,
-      title: this.title,
-      courseID: this.courseID,
-      lectureNumber: this.lectureNumber,
-      description: this.description,
-      instructors: this.instructors,
-      thumbnailURL: this.thumbnailURL,
-      lectureNotesHTML: this.lectureNotesHTML,
-    };
+    return this.data;
   }
 
   save(): Promise<void> {
     return new Promise((resolve, reject) => {
-      saveToFirestore(this.collectionName, this.id, this.toJSON()).then(() => {
-        resolve();
-      });
+      saveToFirestore(this.collectionName, this.id, this.toJSON())
+        .catch((err) => reject(err))
+        .then(() => resolve());
     });
   }
 }
