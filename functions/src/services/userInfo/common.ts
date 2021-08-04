3ab0fd2f06c3d1351f07c8c3ff784f661ed7678a
xtrp <36894883+xtrp@users.noreply.github.com>
2021-08-03 23:16:00 -0400

    Add lazy creation of user info item

--- functions/src/services/userInfo/common.ts
+++ functions/src/services/userInfo/common.ts
@@ -6,19 +6,23 @@ import { GenericObject } from '../../util';
 
 const firestore = admin.firestore();
 
+const defaultUserInfoData: UserInfo['data'] = {
+  favoritedCourseIDs: [],
+  lectureProgress: {},
+};
+
 export const validateReqAndGetUserInfo = async (request: any, response: any, callback: Function) => {
   validateUser(request, response, async (user: GenericObject) => {
     const userID = user.uid;
+    let userInfoObj: UserInfo;
     const query = firestore.collection(UserInfo.collectionName).doc(userID);
     const userInfo = await query.get();
-    if (!userInfo.exists) {
-      response.status(404).json({
-        message: 'User info not found',
-      });
-      return;
+    if (userInfo.exists) {
+      userInfoObj = new UserInfo(userInfo.data() || defaultUserInfoData, userInfo.id);
+    } else {
+      userInfoObj = new UserInfo(defaultUserInfoData, userID);
+      await userInfoObj.save();
     }
-
-    const userInfoObj = new UserInfo(userInfo.data() || {}, userInfo.id);
     callback(userID, userInfoObj);
   });
 };
--- functions/src/services/userInfo/getUserInfo.ts
+++ functions/src/services/userInfo/getUserInfo.ts
@@ -1,28 +1,10 @@
-import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
 
 import UserInfo from '../../models/UserInfo';
-
-const firestore = admin.firestore();
+import { validateReqAndGetUserInfo } from './common';
 
 export const getUserInfo = functions.https.onRequest(async (request, response) => {
-  let { userID } = request.query;
-  if (!userID) {
-    response.status(404).json({
-      message: 'userID not provided',
-    });
-    return;
-  }
-  userID = userID.toString();
-  const query = firestore.collection(UserInfo.collectionName).doc(userID);
-  const userInfo = await query.get();
-  if (!userInfo.exists) {
-    response.status(404).json({
-      message: 'User info not found',
-    });
-    return;
-  }
-
-  const userInfoObj = new UserInfo(userInfo.data() || {}, userInfo.id);
-  response.json(userInfoObj.toJSON());
+  validateReqAndGetUserInfo(request, response, async (userID: string, userInfoObj: UserInfo) => {
+    response.json(userInfoObj.toJSON());
+  });
 });
